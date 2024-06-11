49c7e335c3a6d547ff26726ca2b503a07b74deb5
pratiksinha35 <117023786+pratiksinha35@users.noreply.github.com>
2024-06-11 22:22:53 +0530

    experiment tracker with mlflow added

--- steps/evaluate_model.py
+++ steps/evaluate_model.py
@@ -2,13 +2,17 @@ import logging
 from typing import Tuple
 
 import pandas as pd
+import mlflow # type: ignore
 from sklearn.base import RegressorMixin
 from typing_extensions import Annotated # type: ignore
 from zenml import step # type: ignore
+from zenml.client import Client # type: ignore
 
 from src.evaluation import MSE, RMSE, R2
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(
         model: RegressorMixin,
         X_test: pd.DataFrame,
@@ -29,11 +33,14 @@ def evaluate_model(
     
     mse_class = MSE()
     mse = mse_class.calculate_scores(y_pred=prediction, y_true=y_test)
+    mlflow.log_metric('mse', mse)
     
     r2_class = R2()
     r2_score = r2_class.calculate_scores(y_pred=prediction, y_true=y_test)
+    mlflow.log_metric('r2_score', r2_score)
     
     rmse_class = RMSE()
     rmse = rmse_class.calculate_scores(y_pred=prediction, y_true=y_test)
+    mlflow.log_metric('rmse', rmse)
     
     return r2_score, rmse
--- steps/train_model.py
+++ steps/train_model.py
@@ -1,14 +1,19 @@
 import logging
 
 import pandas as pd
+import mlflow # type: ignore
 from zenml import step # type: ignore
+from zenml.client import Client # type: ignore
+
 
 from src.model_dev import LinearRegressionModel
 from sklearn.base import RegressorMixin
 
 from .config import ModelNameConfig
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(
         X_train: pd.DataFrame,  
         y_train: pd.Series,  
@@ -24,6 +29,7 @@ def train_model(
     try:
         model = None
         if config.model_name == 'LinearRegression':
+            mlflow.sklearn.autolog()
             model = LinearRegressionModel()
             trained_model = model.train(X_train, y_train)
             return trained_model
