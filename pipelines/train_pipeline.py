5b8ef992a827e06ed6cf43d19f197d4f6a24be8c
pratiksinha35 <117023786+pratiksinha35@users.noreply.github.com>
2024-06-11 20:56:11 +0530

    updated training pipeline

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -8,7 +8,7 @@ from steps.evaluate_model import evaluate_model
 @pipeline
 def train_pipeline(data_path: str):
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, y_test)
+    rmse, r2_score = evaluate_model(X_test, y_test)
     
