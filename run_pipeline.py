4e1a92e5c4e419517cf5aec6f236855137460dca
pratiksinha35 <117023786+pratiksinha35@users.noreply.github.com>
2024-06-11 22:46:32 +0530

    added mlflow tracking uri code in pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,7 @@
+from zenml.client import Client #type: ignore
+
 from pipelines.train_pipeline import train_pipeline
 
 if __name__ == '__main__':
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
     train_pipeline(data_path=r'D:\Pratik\Customer Satisfaction MLOps\data\olist_customers_dataset.csv')
