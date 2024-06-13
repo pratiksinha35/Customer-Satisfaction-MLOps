ba856650583796758818a3817f7962358e3ad5af
pratiksinha35 <117023786+pratiksinha35@users.noreply.github.com>
2024-06-13 12:38:38 +0530

    fixed error in pipeline

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -10,5 +10,5 @@ def train_pipeline(data_path: str):
     df = ingest_df(data_path)
     X_train, X_test, y_train, y_test = clean_df(df)
     model = train_model(X_train, y_train)
-    rmse, r2_score = evaluate_model(model, X_test, y_test)
+    r2_score, rmse = evaluate_model(model, X_test, y_test)
     
